863b7ce0c9c4da611dcabf067682f2a7c9f05a90
Vevan O Narain <vevanonarain@outlook.com>
2024-09-26 13:37:58 +0530

    Update news.py

--- backend/news.py
+++ backend/news.py
@@ -1,184 +1,254 @@
-from flask import Flask, jsonify, request
-import requests
-import json
-from flask_cors import CORS
-import yfinance as yf
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential
-from keras.layers import LSTM, Dense, Dropout
-import plotly.graph_objects as go
+import React, { useEffect, useState } from 'react';
+import { 
+  SafeAreaView, 
+  Text, 
+  TextInput, 
+  Button, 
+  View, 
+  ScrollView, 
+  StyleSheet, 
+  TouchableOpacity
+} from 'react-native';
 
-app = Flask(__name__)
-
-# Fetch historical stock data
-def fetch_stock_data(stock_symbol, start_date, end_date):
-    data = yf.download(stock_symbol, start=start_date, end=end_date)
-    return data
-
-# Preprocess the stock data
-def preprocess_data(data):
-    if data.empty:
-        return None, None, None
-
-    data.reset_index(inplace=True)
-    data = data.sort_values('Date')
-    data['Close'] = pd.to_numeric(data['Close'], errors='coerce')
-
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    scaled_data = scaler.fit_transform(data[['Close']])
-
-    X, y = [], []
-    for i in range(60, len(scaled_data)):
-        X.append(scaled_data[i-60:i, 0])
-        y.append(scaled_data[i, 0])
-
-    X, y = np.array(X), np.array(y)
-    X = np.reshape(X, (X.shape[0], X.shape[1], 1))
-
-    return X, y, scaler
-
-# Build the LSTM model
-def build_lstm_model(input_shape):
-    model = Sequential()
-    model.add(LSTM(units=50, return_sequences=True, input_shape=input_shape))
-    model.add(Dropout(0.2))
-    model.add(LSTM(units=50, return_sequences=False))
-    model.add(Dropout(0.2))
-    model.add(Dense(units=1))
-    model.compile(optimizer='adam', loss='mean_squared_error')
-    return model
-
-# Train the model
-def train_model(model, X_train, y_train):
-    model.fit(X_train, y_train, epochs=7, batch_size=32)
-    return model
-
-# Predict the future price
-def predict_future_price(model, data, scaler, future_days):
-    last_60_days = data[-60:].values
-    last_60_days_scaled = scaler.transform(last_60_days)
-
-    X_future = []
-    X_future.append(last_60_days_scaled)
-    X_future = np.array(X_future)
-    X_future = np.reshape(X_future, (X_future.shape[0], X_future.shape[1], 1))
-
-    predicted_price = model.predict(X_future)
-    predicted_price = scaler.inverse_transform(predicted_price)
-
-    return predicted_price[0, 0]
-
-@app.route('/predict', methods=['POST'])
-def predict_stock():
-    data = request.json
-    stock_symbol = data['symbol']
-    years_ahead = data['years']
-
-    start_date = '2017-01-01'
-    end_date = pd.Timestamp.now().strftime('%Y-%m-%d')
-
-    # Fetch stock data
-    stock_data = fetch_stock_data(stock_symbol, start_date, end_date)
-    if stock_data.empty:
-        return jsonify({'error': 'Stock data not found. Please check the symbol.'}), 400
-
-    # Preprocess data
-    X, y, scaler = preprocess_data(stock_data)
-    if X is None:
-        return jsonify({'error': 'Error in preprocessing the data.'}), 400
-
-    # Build and train the model
-    model = build_lstm_model((X.shape[1], 1))
-    model = train_model(model, X, y)
-
-    # Predict future price
-    future_price = predict_future_price(model, stock_data[['Close']], scaler, years_ahead * 365)
-
-    # Convert future_price to Python native float
-    return jsonify({'predicted_price': round(float(future_price), 2)})
+import axios from 'axios';
 
 
+function App() {
+  const [stockSymbol, setStockSymbol] = useState([])
+  const [news, setNews] = useState([]);
+  const [error, setError] = useState('');
 
-@app.route('/plot', methods=['POST'])
-def plot_stock():
-    data = request.json
-    stock_symbol = data['symbol']
+  const [symbol, setSymbol] = useState('');
+  const [years, setYears] = useState(1); 
+  const [predictedPrice, setPredictedPrice] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
 
-    start_date = '2017-01-01'
-    end_date = pd.Timestamp.now().strftime('%Y-%m-%d')
+  const handlePredict = async (e) => {
+    e.preventDefault();
+    setErrorMessage('');  
+    setPredictedPrice(null); 
 
-    stock_data = fetch_stock_data(stock_symbol, start_date, end_date)
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/predict', {
+        symbol: symbol,
+        years: years
+      });
 
-    if stock_data.empty:
-        return jsonify({'error': 'Stock data not found.'}), 400
-
-    try:
-        X, y, scaler = preprocess_data(stock_data)
-        model = build_lstm_model((X.shape[1], 1))
-        model = train_model(model, X, y)
-
-        # Generate the prediction plot
-        scaled_data = scaler.fit_transform(stock_data[['Close']])
-        X_test, y_test = [], []
-        for i in range(60, len(scaled_data)):
-            X_test.append(scaled_data[i-60:i, 0])
-            y_test.append(scaled_data[i, 0])
-
-        X_test = np.array(X_test)
-        X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-
-        predicted_prices = model.predict(X_test)
-        predicted_prices = scaler.inverse_transform(predicted_prices)
-
-        # Create a Plotly figure
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=stock_data['Date'], y=stock_data['Close'], mode='lines', name='Actual Prices'))
-        fig.add_trace(go.Scatter(x=stock_data['Date'][60:], y=predicted_prices.flatten(), mode='lines', name='Predicted Prices'))
-
-        fig.update_layout(title='Stock Price Prediction with LSTM',
-                          xaxis_title='Date',
-                          yaxis_title='Price in $',
-                          hovermode='x unified')
-
-        return fig.to_json()
-
-    except ValueError as e:
-        return jsonify({'error': str(e)}), 400
+      if (response.data && response.data.predicted_price) {
+        setPredictedPrice(response.data.predicted_price);
+      } else {
+        setErrorMessage('No prediction data available.');
+      }
+    } catch (err) {
+      setErrorMessage('Error fetching the prediction. Please check the stock symbol.');
+    }
+  };
 
 
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:5000/news');
+        setNews(response.data);
+      } catch (err) {
+        setError('Failed to fetch stock market news');
+      }
+    };
 
-CORS(app)  # To handle CORS between Flask and React
+    fetchNews();
+  }, []);
+  return (
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity style = {styles.header}>
+      <Text style={styles.heading}>Trade Forecast</Text>
+      </TouchableOpacity>
 
-# Replace with your actual API key and endpoint
-API_KEY = '3bc255f066af4f0781a2df90d96258b3'  # Get this from a service like NewsAPI
-NEWS_API_URL = 'https://newsapi.org/v2/top-headlines'
 
-@app.route('/news', methods=['GET'])
-def get_stock_market_news():
-    try:
-        # Make a request to the news API for stock market news
-        response = requests.get(
-            NEWS_API_URL,
-            params={
-                'category': 'business',
-                'q': 'stock market',     # Or search specific keywords
-                'apiKey': '3bc255f066af4f0781a2df90d96258b3',       # Your API key
-                'language': 'en',
-                'sortBy': 'publishedAt'
-            }
-        )
-        news_data = response.json()
+      <View style = {{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+        <View>
+        <div 
+        style = {{width: 600, height: 533, justifyContent: 'center'}}>
+        <View>
+        <View>
+          <img src = 'https://cdn.pixabay.com/photo/2024/01/06/02/44/ai-generated-8490532_640.png' style = {{width: 600, height: 350, marginLeft: 20, marginTop: 30}}/></View>
+        <View style = {{marginTop: 10, marginLeft: 50}}>
+            <div>
+            <form onSubmit={handlePredict} style = {{marginLeft: 100, justifyContent: 'center'}}>
+              <div style = {{marginBottom: 10, marginTop: 10}}>
+                <label style = {{fontSize: 20, fontFamily: 'Verdana'}}>Stock Symbol: </label>
+                <input
+                  type="text"
+                  value={symbol}
+                  onChange={(e) => setSymbol(e.target.value)}
+                  placeholder="AAPL, TSLA, etc."
+                  required
+                />
+              </div>
+              <div style = {{marginBottom: 10}}>
+                <label style = {{fontSize: 20, fontFamily: 'Verdana'}}>Years Ahead: </label>
+                <input
+                  type="number"
+                  value={years}
+                  onChange={(e) => setYears(e.target.value)}
+                  min="1"
+                  required
+                />
+              </div>
+              <button type="submit" style = {{height: 50,width: 300 ,justifyContent: 'center', borderRadius: 5, backgroundColor: '#C4DAD2', marginLeft: 10}}>
+                <Text  style = {{alignSelf: 'center', fontFamily: 'Verdana', fontSize: 28}}>Predict</Text>
+                </button>
+            </form>
 
-        # Check for errors
-        if response.status_code != 200 or 'articles' not in news_data:
-            return jsonify({'error': 'Could not fetch stock market news'}), 500
+            <div style={{ marginLeft: 50 }}>
+            {predictedPrice ? (
+              <h2>Predicted Price after {years} years: ${predictedPrice}</h2>
+              ) : (
+              <h5>(ETA: 15s)</h5>
+            )}
+  </div>
 
-        return jsonify(news_data['articles']), 200
+            {errorMessage && (
+              <div style={{ color: 'red' }}>
+                <p>{errorMessage}</p>
+              </div>
+            )}
+          </div>
+        </View>
+      </View>
 
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        </div>
+        </View>
 
-if __name__ == '__main__':
-    app.run(debug=True)
+      <View style = {{width: 100}}></View>
+
+    <View style = {{flex: 1, flexDirection: 'column', justifyContent: 'center', marginTop: 20}}>
+      <div>
+        <View style = {{flex: 1, marginTop: 10}}>
+        <TouchableOpacity style = {{height: 40}}><Text style = {{fontSize: 32, textAlign: 'center', fontFamily: 'Verdana'}}><u>Stock Market News</u></Text></TouchableOpacity>
+        </View>
+       <ScrollView style = {{height: 310}}> 
+    
+    <View>
+
+
+      {error && <p>{error}</p>}
+
+      <div style = {{marginTop: 10}}>
+        {news.map((article, index) => (
+          <div key={index} style={{ marginBottom: 20 }}>
+            <p style = {{fontFamily: 'Verdana'}}>{index+ 1}) {article.title}</p>
+            <p style = {{fontFamily: 'Verdana'}}>{article.description}</p>
+            <a href={article.url} target="_blank" rel="noopener noreferrer" style = {{fontFamily: 'Verdana'}}>
+              Read more
+            </a>
+          </div>
+        ))}
+      </div>
+    </View>
+    </ScrollView>
+    </div>
+    <View style = {{height: 30, marginBottom: 20, borderBottomWidth: 1, width: 510}}></View>
+    <View>
+    <TouchableOpacity style = {{height: 40}}><Text style = {{fontSize: 32, textAlign: 'center', fontFamily: 'Verdana'}}><u>Stock Market Apps</u></Text></TouchableOpacity>
+  <View style = {{flex: 1, flexDirection: 'row', marginTop: 50}}>
+    <View>      
+  <a href="https://www.webull.com/" target='_blank'><img src="https://play-lh.googleusercontent.com/WXbQNRz-G6P16seMX9vtBhzYPWPIbmyQRsn-RZOoQ5mI3WmqRcZxwUkPX15lwWpvaaY" style = {{width: 70, height: 70, padding: 9, marginLeft: 45}}/></a>
+  <Text style = {{padding: 5, textAlign: 'center', marginLeft: 43}}>Webull</Text>
+  </View>
+  <View>
+  <a href="https://www.5paisa.com/" target='_blank'><img src="https://play-lh.googleusercontent.com/-MNspIooJf9VXXApEo8Z1eaokA5k5Be7TcgKeWSwBZGTRcsKZTVWkEcCzHq5ntAcsI0" style = {{width: 70, height: 70, padding: 9}}/></a>
+  <Text style = {{padding: 5, textAlign: 'center'}}>5 Paisa</Text>
+  </View>
+  <View>
+  <a href="https://www.angelone.in/" target='_blank'><img src="https://cdn.brandfetch.io/angelbroking.com/fallback/transparent/theme/dark/h/512/w/512/icon?t=1717818058774" style = {{width: 70, height: 70, padding: 9}} /></a>
+  <Text style = {{padding: 5, textAlign: 'center'}}>Angel One</Text>
+  </View>
+  <View>
+  <a href="https://groww.in/" target='_blank'><img src="https://logowik.com/content/uploads/images/groww1643.logowik.com.webp" style = {{width: 70, height: 70, padding: 9}}/></a>
+  <Text style = {{padding: 5, textAlign: 'center'}}>Groww</Text>
+  </View>
+  <View>
+  <a href="https://upstox.com/" target='_blank'><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWPSWjViJCWKR3opI6fnHvUVLAls14aN50NQ&s" style = {{width: 70, height: 70, padding: 9}}/></a>
+  <Text style = {{padding: 5, textAlign: 'center'}}>Upstox</Text>
+  </View>
+  </View>
+    </View>
+    </View>
+    </View>
+
+    <View>
+    <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjRs7L_5d2IAxX84TgGHWjFMIUQFnoECBcQAQ&url=https%3A%2F%2Fkb.icai.org%2Fpdfs%2FPDFFile5b28c9ce64e524.54675199.pdf&usg=AOvVaw00rsDfJ9_kGvSsG_ZIQepK&opi=89978449 " target="_blank" rel="Rules and Clauses for investing in stock market">
+        Government Policy
+      </a>
+    </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: '#E9EFEC',
+    width: 1224.4
+  },
+  heading: {
+    fontSize: 28,  
+    fontWeight: 'bold',
+    color: '#fff',  
+    textAlign: 'center', 
+    textShadowColor: 'rgba(0, 0, 0, 0.25)', 
+    fontFamily: 'Verdana',
+    fontSize: 40
+  },
+  header:{
+    backgroundColor: '#16423C',
+    height: 80,
+    justifyContent: 'center',
+  },
+  inputContainer: {
+    marginBottom: 20,
+    marginTop: 20,
+    alignSelf: 'center',
+    width: 250,
+  },
+  input: {
+    borderBottomWidth: 2,
+    borderColor: '#00000',
+    padding: 10,
+    marginVertical: 10,
+    width: 250,
+    alignSelf: 'center',
+    justifyContent: 'center'
+  },
+  subHeading: {
+    fontSize: 18,
+    marginVertical: 10,
+    fontWeight: 'bold',
+  },
+  predictedPrice: {
+    fontSize: 20,
+    color: '#3178bf',
+    marginBottom: 20,
+  },
+  noPrediction: {
+    fontSize: 16,
+    color: '#555',
+    fontFamily: 'Verdana'
+  },
+  newsItem: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+    paddingVertical: 10,
+  },
+  newsTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    fontFamily: 'Verdana'
+  },
+  link: {
+    color: '#007bff',
+    marginTop: 5,
+  },
+});
+
+export default App
